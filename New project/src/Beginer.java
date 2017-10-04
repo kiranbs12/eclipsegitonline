7cbb7da9e698689bb27f4d0a92b0c25551ceaa22
kiranbsoudi.soudi <kiranbsoudi@infosys.com>
2017-10-04 19:02:18 +0530

    create local repository on your desktop in git

---
+++ New project/src/Beginer.java
@@ -0,0 +1,14 @@
+
+public class Beginer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=90;
+		int b=63;
+		//float c;
+		int c= a+b;
+		System.out.println("sum:"+c);
+		System.out.println("i am sorry");
+	}
+
+}
